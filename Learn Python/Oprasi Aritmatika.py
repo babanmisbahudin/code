ac392312e7bcbe0c7faff5f4595a9e2e6f71c142
Baban Misbahudin <babanmisbahudin200@gmail.com>
2025-06-18 02:12:51 +0000

    commit

--- Learn Python/Oprasi Aritmatika.py
+++ Learn Python/Oprasi Aritmatika.py
@@ -3,28 +3,28 @@ b = 3
 
 # Oprasi Penambahan +
 hasil = a + b
-print("OPERASI PENAMBAHAN",a,'+',b,'=',hasil)
+print("OPERASI PENAMBAHAN ---",a,'+',b,'=',hasil)
 
 # Oprasi Pengurangan -
 hasil = a - b
-print("OPERASI PENGURANGAN",a,'-',b,'=',hasil)
+print("OPERASI PENGURANGAN ---",a,'-',b,'=',hasil)
 
 # Oprasi Perkalian *
 hasil = a * b
-print("OPERASI PERKALIAN",a,'*',b,'=',hasil)
+print("OPERASI PERKALIAN ---",a,'*',b,'=',hasil)
 
 # Oprasi Perbagian /
 hasil = a / b
-print("OPERASI PEMBAGIAN",a,'/',b,'=',hasil)
+print("OPERASI PEMBAGIAN ---",a,'/',b,'=',hasil)
 
 # Oprasi Eksponen (Pangkat) **
 hasil = a ** b
-print("OPERASI EKSPONEN",a,'**',b,'=',hasil)
+print("OPERASI EKSPONEN ---",a,'**',b,'=',hasil)
 
 # Oprasi Modulus (sisa pembagian) %
 hasil = a % b
-print("OPERASI MODULUS",a,'%',b,'=',hasil)
+print("OPERASI MODULUS ---",a,'%',b,'=',hasil)
 
 # Operasi Floor Devision //
 hasil = a // b
-print("OPERASI FLOOR DEVISIOM",a,'//',b,'=',hasil)
+print("OPERASI FLOOR DEVISIOM ---",a,'//',b,'=',hasil)
