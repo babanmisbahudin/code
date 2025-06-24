0ac3d43b0353108f965b233a3e7e5c1b813db697
Baban Misbahudin <babanmisbahudin200@gmail.com>
2025-06-24 02:21:06 +0000

    commit

--- Learn Python/Komperasi Logika & Boolean.py
+++ Learn Python/Komperasi Logika & Boolean.py
@@ -1,6 +1,11 @@
 # Membuat gabungan area rentang dan angka
 
 inputUser = float(input("Masukkan angka yang bernilai\nkurang dari 3 \natau \nlebih besar dari 10:"))
+
 # Memeriksa angka kurang dari 3
 isKurangDari = (inputUser < 3)
 print(isKurangDari)
+
+# Memeriksa angka lebih besar dari 10
+isLebihBesarDari = (inputUser > 10)
+print(isLebihBesarDari)
