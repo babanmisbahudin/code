a8983981dd1c40fc72d3de4c97008be9061c1c78
Baban Misbahudin <babanmisbahudin200@gmail.com>
2025-06-30 08:18:55 +0000

    commit

---
+++ Learn Python/String.py
@@ -0,0 +1,26 @@
+data = "ini adalah string"
+print(data)
+print(type(data))
+
+# Cara menggunakan string
+'''
+    1. dengan menggunakan tanda petik satu / single qoute '...'
+    2. dengan menggunakan tanda petik dua / double qoute "..."
+'''
+
+data1 = 'ini adalah string dengan single quote'
+print(data1)
+data2 = "ini adalah string dengan double quote"
+print(data2) 
+
+# Contoh penggunaan string lainnya
+print("'Halo apa kabar?'")
+print('"Halo apa kabar?"')
+print('''Halo apa kabar?
+    Ini adalah contoh string dengan triple quote
+    Triple quote bisa digunakan untuk string multi line
+''')
+print("ini adalah hari yang jum'at")
+
+# Contoh menggunakan tanda  \
+print('mari sholat jum\'at')
